1a991007ef897183fa059693b6f0d05f65e1a966
Denis Chumachenko <deniskachumachenko1@gmail.com>
2023-01-25 02:07:23 +0200

    Set up eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['next/core-web-vitals', 'plugin:react/recommended', 'airbnb', 'airbnb-typescript', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json']
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/prop-types': 'off',
+    'import/extensions': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ]
+  }
+};
